195bde07e784ffe3a6a430185ca5cdeb893d96fe
Shamsuzzoha <33594007+ArbiKhan007@users.noreply.github.com>
2025-11-19 10:05:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function volume_sphere() {
-    //Write your code here
-  
+function volume_sphere(e) {
+	e.preventDefault();
+	let vol="";
+	let rad=parseInt(document.querySelector("#radius").value);
+	if(rad>=0) vol=(4/3)*Math.PI*rad*rad*rad;
+	else vol=NaN;
+	console.log(vol);
+	document.querySelector("#volume").value=vol;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
